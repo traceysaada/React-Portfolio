9e3627d6e0d04e9e09c0d0e02e58c3c155f995ea
Tracey Saada <tracey.saada@outlook.com>
2021-04-02 19:49:19 +0100

    Added CV to About

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,10 +1,13 @@
 import React from "react";
+import {Button} from "react-bootstrap"
 
 export default function About ()
 {
  return (
-     <h1>About</h1>
-
-     //
+   
+<>
+<hr />
+             <a href="https://drive.google.com/file/d/12vzjvLG0JS56tjWpSWp03YnXUGLwN8Y9/view?usp=sharing" target="_blank" rel="noreferrer"><Button variant="outline-info">View my CV</Button></a>
+ </>
  )
 }
